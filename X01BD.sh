425174e609e52ca0497ba5bba2337d19ac6cd552
Najah Muhabbik <72481862+frostid@users.noreply.github.com>
2021-02-22 23:17:23 +0700

    v1.1

--- X01BD.sh
+++ X01BD.sh
@@ -39,8 +39,8 @@ KERNEL_DIR=$PWD
 KERNEL="Moonlight"
 
 # Kernel zip name type
-export LOCALVERSION="Tsukuyomi-v1.0"
-LOCALVERSION="Tsukuyomi-v1.0"
+export LOCALVERSION="Tsukuyomi-v1.1"
+LOCALVERSION="Tsukuyomi-v1.1"
 
 # The name of the device for which the kernel is built
 MODEL="Asus Zenfone Max Pro M2"
