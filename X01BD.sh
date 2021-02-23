a764c5ad57a733450a5bd61c75155c8fe8884c90
Najah Muhabbik <72481862+frostid@users.noreply.github.com>
2021-02-23 23:52:15 +0700

    v1.1 - Hikari

--- X01BD.sh
+++ X01BD.sh
@@ -39,8 +39,8 @@ KERNEL_DIR=$PWD
 KERNEL="Moonlight"
 
 # Kernel zip name type
-export LOCALVERSION="Tsukuyomi-v1.1"
-LOCALVERSION="Tsukuyomi-v1.1"
+export LOCALVERSION="Hikari-v1.1"
+LOCALVERSION="Hikari-v1.1"
 
 # The name of the device for which the kernel is built
 MODEL="Asus Zenfone Max Pro M2"
